5f126f4c4c281a39740174030eb91531cbccd2ca
RahulVBharadwaj <76405542+bharawdwajrahul@users.noreply.github.com>
2021-11-14 19:18:37 +0530

    Update Covid_DataSet_SQLQuery1.sql

--- Covid_DataSet_SQLQuery1.sql
+++ Covid_DataSet_SQLQuery1.sql
@@ -9,7 +9,7 @@ SELECT location,date, total_cases, new_cases,total_deaths, population
 FROM portfolio_project..CovidDeaths
 order by 1, 2
 
---Shows likelyhood of dying if you contact covid in your country
+--Shows likelihood of dying if you contact covid in your country
 SELECT location,date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 FROM portfolio_project..CovidDeaths
 Where location = 'India'
